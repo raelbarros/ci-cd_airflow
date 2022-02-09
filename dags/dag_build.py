66812c3396efe1b037d483eca253d35b7aace5d9
Israel Barros <israelb@sauter.digital>
2022-02-09 18:11:35 -0300

    teste build

---
+++ dags/dag_build.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+# TESTE CLOUD BUILD
+# TESTE 02
+
+default_args = {
+    "owner": "data-engineering",
+    "start_date": datetime(2022, 1, 1),
+    "email": ["israelb@sauter.digital"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=10),
+}
+
+dag = DAG(
+    "dag_build",
+    default_args=default_args,
+    schedule_interval="*/2 * * * *",
+    catchup=False,
+)
+
+
+def hello_world():
+    print("hello_world")
+
+
+task_fail = PythonOperator(
+    task_id="task_fail", python_callable=hello_world, dag=dag
+)
